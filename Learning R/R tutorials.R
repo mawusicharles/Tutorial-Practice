69affa8972dbb3ad0ee63f7db45f715917df8bd7
mawusicharles <mawusicharles@yahoo.com>
2021-04-18 02:09:52 +0200

    subsetting data

---
+++ Learning R/R tutorials.R
@@ -0,0 +1,9 @@
+# Charles Mawusi 
+# Week 2 Tutorials
+install.packages("gapminder")
+install.packages("Rtools")
+library("gapminder")
+help("gapminder")
+str(gapminder)
+colSums(is.na(gapminder))
+# Subsetting data using tidyvers and dplyr 
